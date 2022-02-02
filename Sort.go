cde34d4bc69a97f8bdf940dbfecfa2c13c21208d
mingkeey <17633808208@163.com>
2022-02-02 22:34:16 +0800

    ShellSort

--- Sort.go
+++ Sort.go
@@ -118,8 +118,19 @@ func ShellSort(list []int) {
 	// 设置步长每次减半，直到1
 	for step := n / 2; step >= 1; step /= 2 {
 		// 开始插入排序
+		// 取 d = 6 对 [5 x x x x x 6 x x x x x] 进行直接插入排序，没有变化。
+		// 取 d = 3 对 [5 x x 6 x x 6 x x 4 x x] 进行直接插入排序，排完序后：[4 x x 5 x x 6 x x 6 x x]
+		// 取 d = 1 对 [4 9 1 5 8 14 6 49 25 6 6 3] 进行直接插入排序，因为 d=1 完全就是直接插入排序了
 		for i := step; i < n; i += step {
-			
+			// 固定步长
+			// 比较
+			for j := i - step; j >= 0; j -= step {
+				if list[j+step] < list[j] {
+					list[j], list[j+step] = list[j+step], list[j]
+					continue
+				}
+				break
+			}
 
 		}
 	}
