60506b2156922c49cd7d5fd9c9a0a11ab64b7929
mingkeey <17633808208@163.com>
2022-02-03 20:50:50 +0800

    commit

--- LinkStack.go
+++ LinkStack.go
@@ -1,72 +1,72 @@
 package main
 
-import (
-	// "fmt"
-	"sync"
-)
+// import (
+// 	// "fmt"
+// 	"sync"
+// )
 
-type LinkNode4Stack struct {
-	Next  *LinkNode4Stack
-	Value interface{}
-}
+// type LinkNode4Stack struct {
+// 	Next  *LinkNode4Stack
+// 	Value interface{}
+// }
 
-type LinkStack struct {
-	root *LinkNode4Stack
-	size int
-	lock sync.Mutex
-}
+// type LinkStack struct {
+// 	root *LinkNode4Stack
+// 	size int
+// 	lock sync.Mutex
+// }
 
-func (stack *LinkStack) Push(e interface{}) {
-	stack.lock.Lock()
-	defer stack.lock.Unlock()
-	newNode := new(LinkNode4Stack)
-	newNode.Value = e
-	if stack.root != nil {
-		currentNode := stack.root
-		newNode.Next = currentNode
-	}
-	stack.root = newNode
-	stack.size = stack.size + 1
-}
+// func (stack *LinkStack) Push(e interface{}) {
+// 	stack.lock.Lock()
+// 	defer stack.lock.Unlock()
+// 	newNode := new(LinkNode4Stack)
+// 	newNode.Value = e
+// 	if stack.root != nil {
+// 		currentNode := stack.root
+// 		newNode.Next = currentNode
+// 	}
+// 	stack.root = newNode
+// 	stack.size = stack.size + 1
+// }
 
-// 出栈
-func (stack *LinkStack) Pop() interface{} {
-	stack.lock.Lock()
-	defer stack.lock.Unlock()
-	// 栈中元素已空
-	if stack.size == 0 {
-		panic("empty")
-	}
-	// 顶部元素要出栈
-	topNode := stack.root
-	v := topNode.Value
-	// 将顶部元素的后继链接链上
-	stack.root = topNode.Next
-	// 栈中元素数量-1
-	stack.size = stack.size - 1
-	return v
-}
+// // 出栈
+// func (stack *LinkStack) Pop() interface{} {
+// 	stack.lock.Lock()
+// 	defer stack.lock.Unlock()
+// 	// 栈中元素已空
+// 	if stack.size == 0 {
+// 		panic("empty")
+// 	}
+// 	// 顶部元素要出栈
+// 	topNode := stack.root
+// 	v := topNode.Value
+// 	// 将顶部元素的后继链接链上
+// 	stack.root = topNode.Next
+// 	// 栈中元素数量-1
+// 	stack.size = stack.size - 1
+// 	return v
+// }
 
-// 获取栈顶元素
-func (stack *LinkStack) Peek() interface{} {
-	// 栈中元素已空
-	if stack.size == 0 {
-		panic("empty")
-	}
+// // 获取栈顶元素
+// func (stack *LinkStack) Peek() interface{} {
+// 	// 栈中元素已空
+// 	if stack.size == 0 {
+// 		panic("empty")
+// 	}
 	
-	// 顶部元素值
-	v := stack.root.Value
-	return v
-} // 栈大小
-func (stack *LinkStack) Size() int {
-	return stack.size
-}
+// 	// 顶部元素值
+// 	v := stack.root.Value
+// 	return v
+// } // 栈大小
+// func (stack *LinkStack) Size() int {
+// 	return stack.size
+// }
 
-// 栈是否为空
-func (stack *LinkStack) IsEmpty() bool {
+// // 栈是否为空
+// func (stack *LinkStack) IsEmpty() bool {
 	
-	return stack.size == 0
-}
+// 	return stack.size == 0
+// }
 // func main() {
 // 	linkStack := new(LinkStack)
 // 	linkStack.Push("cat")
--- Set.go
+++ Set.go
@@ -1,91 +1,91 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+// import (
+// 	"fmt"
+// 	"sync"
+// )
 
-type Set struct {
-	m   map[interface{}]struct{}
-	len int
-	sync.RWMutex
-}
+// type Set struct {
+// 	m   map[interface{}]struct{}
+// 	len int
+// 	sync.RWMutex
+// }
 
-func NewSet(cap int) *Set {
-	temp := make(map[interface{}]struct{}, cap)
-	return &Set{
-		m: temp,
-	}
-}
+// func NewSet(cap int) *Set {
+// 	temp := make(map[interface{}]struct{}, cap)
+// 	return &Set{
+// 		m: temp,
+// 	}
+// }
 
-func (s *Set) Add(e interface{}) {
-	s.Lock()
-	defer s.Unlock()
-	s.m[e] = struct{}{}
-	s.len = len(s.m)
-}
+// func (s *Set) Add(e interface{}) {
+// 	s.Lock()
+// 	defer s.Unlock()
+// 	s.m[e] = struct{}{}
+// 	s.len = len(s.m)
+// }
 
-func (s *Set) Remove(e interface{}) {
-	s.Lock()
-	defer s.Unlock()
-	if s.len == 0 {
-		return
-	}
-	delete(s.m, e)
-	s.len = len(s.m)
-}
+// func (s *Set) Remove(e interface{}) {
+// 	s.Lock()
+// 	defer s.Unlock()
+// 	if s.len == 0 {
+// 		return
+// 	}
+// 	delete(s.m, e)
+// 	s.len = len(s.m)
+// }
 
-func (s *Set) Has(e interface{}) bool {
-	s.Lock()
-	defer s.Unlock()
-	_, ok := s.m[e]
-	return ok
-}
+// func (s *Set) Has(e interface{}) bool {
+// 	s.Lock()
+// 	defer s.Unlock()
+// 	_, ok := s.m[e]
+// 	return ok
+// }
 
-func (s *Set) Len() int {
-	return s.len
-}
+// func (s *Set) Len() int {
+// 	return s.len
+// }
 
-func (s *Set) IsEmpty() bool {
-	return s.len == 0
-}
+// func (s *Set) IsEmpty() bool {
+// 	return s.len == 0
+// }
 
-func (s *Set) Clear() {
-	s.Lock()
-	defer s.Unlock()
-	s.m = map[interface{}]struct{}{}
-	s.len = 0
-}
+// func (s *Set) Clear() {
+// 	s.Lock()
+// 	defer s.Unlock()
+// 	s.m = map[interface{}]struct{}{}
+// 	s.len = 0
+// }
 
-func (s *Set) List() []interface{} {
-	s.RLock()
-	defer s.RUnlock()
-	list := make([]interface{}, 0, s.len)
-	for k := range s.m {
-		list = append(list, k)
-	}
-	return list
+// func (s *Set) List() []interface{} {
+// 	s.RLock()
+// 	defer s.RUnlock()
+// 	list := make([]interface{}, 0, s.len)
+// 	for k := range s.m {
+// 		list = append(list, k)
+// 	}
+// 	return list
 
-}
-func main() {
-	//other()
-	// 初始化一个容量为5的不可重复集合
-	s := NewSet(5)
-	s.Add(1)
-	s.Add(1)
-	s.Add(2)
-	fmt.Println("list of all items", s.List())
-	s.Clear()
-	if s.IsEmpty() {
-		fmt.Println("empty")
-	}
-	s.Add(1)
-	s.Add(2)
-	s.Add(3)
-	if s.Has(2) {
-		fmt.Println("2 does exist")
-	}
-	s.Remove(2)
-	s.Remove(3)
-	fmt.Println("list of all items", s.List())
-}
+// }
+// func main() {
+// 	//other()
+// 	// 初始化一个容量为5的不可重复集合
+// 	s := NewSet(5)
+// 	s.Add(1)
+// 	s.Add(1)
+// 	s.Add(2)
+// 	fmt.Println("list of all items", s.List())
+// 	s.Clear()
+// 	if s.IsEmpty() {
+// 		fmt.Println("empty")
+// 	}
+// 	s.Add(1)
+// 	s.Add(2)
+// 	s.Add(3)
+// 	if s.Has(2) {
+// 		fmt.Println("2 does exist")
+// 	}
+// 	s.Remove(2)
+// 	s.Remove(3)
+// 	fmt.Println("list of all items", s.List())
+// }
