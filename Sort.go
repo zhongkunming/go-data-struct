5b71ae661b5836fb91bb1c055ff94f35faaa7ab0
mingkeey <17633808208@163.com>
2022-02-03 20:26:59 +0800

    快速排序

--- Sort.go
+++ Sort.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
@@ -24,7 +25,11 @@ func main() {
 	// for range list {
 	// 	h.Pop()
 	// }
-	HeapSort(list)
+	// HeapSort(list)
+	// QuickSort(list, 0, len(list)-1)
+	// QuickSort1(list, 0, len(list)-1)
+	// QuickSort3(list, 0, len(list)-1)
+	QuickSort4(list, 0, len(list)-1)
 	fmt.Println(list)
 }
 
@@ -129,7 +134,7 @@ func InsertSort(list []int) {
 	}
 }
 
-// 把不那么有序的列表变得相对有序， 不同步长试用插入排序
+// 把不那么有序的列表变得相对有序， 不同步长用插入排序
 func ShellSort(list []int) {
 	n := len(list)
 	// 每次步长减半
@@ -361,38 +366,267 @@ func HeapSort(list []int) {
 		start--
 	}
 	// 下沉结束 排序
-    // 元素大于2个的最大堆才可以移除
-    for end > 0 {
-        // 将堆顶元素与堆尾元素互换，表示移除最大堆元素
-        list[end], list[0] = list[0], list[end]
-        // 对堆顶进行下沉操作
-        sift(list, 0, end)
-        // 一直移除堆顶元素
-        end--
-    }
+	// 元素大于2个的最大堆才可以移除
+	for end > 0 {
+		// 将堆顶元素与堆尾元素互换，表示移除最大堆元素
+		list[end], list[0] = list[0], list[end]
+		// 对堆顶进行下沉操作
+		sift(list, 0, end)
+		// 一直移除堆顶元素
+		end--
+	}
 }
 
 // 下沉操作，需要下沉的元素时 array[start]，参数 count 只要用来判断是否到底堆底，使得下沉结束
 func sift(array []int, start, count int) {
-    // 父亲节点
-    root := start
-    // 左儿子
-    child := root*2 + 1
-    // 如果有下一代
-    for child < count {
-        // 右儿子比左儿子大，那么要翻转的儿子改为右儿子
-        if count-child > 1 && array[child] < array[child+1] {
-            child++
-        }
-        // 父亲节点比儿子小，那么将父亲和儿子位置交换
-        if array[root] < array[child] {
-            array[root], array[child] = array[child], array[root]
-            // 继续往下沉
-            root = child
-            child = root*2 + 1
+	// 父亲节点
+	root := start
+	// 左儿子
+	child := root*2 + 1
+	// 如果有下一代
+	for child < count {
+		// 右儿子比左儿子大，那么要翻转的儿子改为右儿子
+		if count-child > 1 && array[child] < array[child+1] {
+			child++
+		}
+		// 父亲节点比儿子小，那么将父亲和儿子位置交换
+		if array[root] < array[child] {
+			array[root], array[child] = array[child], array[root]
+			// 继续往下沉
+			root = child
+			child = root*2 + 1
+		} else {
+			return
+		}
+	}
+}
+
+func QuickSort(array []int, begin, end int) {
+	if begin < end {
+		loc := partition(array, begin, end)
+		QuickSort(array, begin, loc-1)
+		QuickSort(array, loc+1, end)
+	}
+}
+
+func partition(array []int, begin, end int) int {
+	i := begin + 1 // 以array[begin]做为基数
+	j := end
+
+	for i < j {
+		if array[i] > array[begin] {
+			array[i], array[j] = array[j], array[i] // 交换
+			j--
+		} else {
+			i++
+		}
+	}
+	/* 跳出while循环后，i = j。
+	 * 此时数组被分割成两个部分  -->  array[begin+1] ~ array[i-1] < array[begin]
+	 *                        -->  array[i+1] ~ array[end] > array[begin]
+	 * 这个时候将数组array分成两个部分，再将array[i]与array[begin]进行比较，决定array[i]的位置。
+	 * 最后将array[i]与array[begin]交换，进行两个分割部分的排序！以此类推，直到最后i = j不满足条件就退出！
+	 */
+	if array[i] >= array[begin] { // 这里必须要取等“>=”，否则数组元素由相同的值组成时，会出现错误！
+		i--
+	}
+	array[begin], array[i] = array[i], array[begin]
+	return i
+}
+
+// 小规模用插入排序
+func QuickSort1(array []int, begin, end int) {
+	if begin < end {
+		// 当数组小于 4 时使用直接插入排序
+		if end-begin <= 4 {
+			InsertSort(array[begin : end+1])
+			return
+		}
+		// 进行切分
+		loc := partition(array, begin, end)
+		// 对左部分进行快排
+		QuickSort1(array, begin, loc-1)
+		// 对右部分进行快排
+		QuickSort1(array, loc+1, end)
+	}
+}
+
+// 三切分的快速排序
+func QuickSort3(array []int, begin, end int) {
+	if begin < end {
+		lt, gt := partition3(array, begin, end)
+		QuickSort(array, begin, lt-1)
+		QuickSort(array, gt+1, end)
+	}
+}
+
+func partition3(array []int, begin, end int) (int, int) {
+    lt := begin       // 左下标从第一位开始
+    gt := end         // 右下标是数组的最后一位
+    i := begin + 1    // 中间下标，从第二位开始
+    v := array[begin] // 基准数
+    // 以中间坐标为准
+    for i <= gt {
+        if array[i] > v { // 大于基准数，那么交换，右指针左移
+            array[i], array[gt] = array[gt], array[i]
+            gt--
+        } else if array[i] < v { // 小于基准数，那么交换，左指针右移
+            array[i], array[lt] = array[lt], array[i]
+            lt++
+            i++
         } else {
-            return
+            i++
+        }
+    }
+    return lt, gt
+}
+
+// 伪尾递归快速排序 因为for循环的存在，不属于递归
+func QuickSort4(array []int, begin, end int) {
+    for begin < end {
+        loc := partition(array, begin, end)
+        // 那边元素少先排哪边
+        if loc-begin < end-loc {
+            // 先排左边
+            QuickSort4(array, begin, loc-1)
+            begin = loc + 1
+        } else {
+            // 先排右边
+            QuickSort4(array, loc+1, end)
+            end = loc - 1
         }
     }
 }
 
+// 链表栈，后进先出
+type LinkStack struct {
+    root *LinkNode  // 链表起点
+    size int        // 栈的元素数量
+    lock sync.Mutex // 为了并发安全使用的锁
+}
+// 链表节点
+type LinkNode struct {
+    Next  *LinkNode
+    Value int
+}
+// 入栈
+func (stack *LinkStack) Push(v int) {
+    stack.lock.Lock()
+    defer stack.lock.Unlock()
+    // 如果栈顶为空，那么增加节点
+    if stack.root == nil {
+        stack.root = new(LinkNode)
+        stack.root.Value = v
+    } else {
+        // 否则新元素插入链表的头部
+        // 原来的链表
+        preNode := stack.root
+        // 新节点
+        newNode := new(LinkNode)
+        newNode.Value = v
+        // 原来的链表链接到新元素后面
+        newNode.Next = preNode
+        // 将新节点放在头部
+        stack.root = newNode
+    }
+    // 栈中元素数量+1
+    stack.size = stack.size + 1
+}
+// 出栈
+func (stack *LinkStack) Pop() int {
+    stack.lock.Lock()
+    defer stack.lock.Unlock()
+    // 栈中元素已空
+    if stack.size == 0 {
+        panic("empty")
+    }
+    // 顶部元素要出栈
+    topNode := stack.root
+    v := topNode.Value
+    // 将顶部元素的后继链接链上
+    stack.root = topNode.Next
+    // 栈中元素数量-1
+    stack.size = stack.size - 1
+    return v
+}
+// 栈是否为空
+func (stack *LinkStack) IsEmpty() bool {
+    return stack.size == 0
+}
+// 非递归快速排序 （不占用程序栈）
+func QuickSort5(array []int) {
+    // 人工栈
+    helpStack := new(LinkStack)
+    // 第一次初始化栈，推入下标0，len(array)-1，表示第一次对全数组范围切分
+    helpStack.Push(len(array) - 1)
+    helpStack.Push(0)
+    // 栈非空证明存在未排序的部分
+    for !helpStack.IsEmpty() {
+        // 出栈，对begin-end范围进行切分排序
+        begin := helpStack.Pop() // 范围区间左边
+        end := helpStack.Pop()   // 范围
+        // 进行切分
+        loc := partition(array, begin, end)
+        // 右边范围入栈
+        if loc+1 < end {
+            helpStack.Push(end)
+            helpStack.Push(loc + 1)
+        }
+        // 左边返回入栈
+        if begin < loc-1 {
+            helpStack.Push(loc - 1)
+            helpStack.Push(begin)
+        }
+    }
+}
+// 非递归快速排序优化
+func QuickSort6(array []int) {
+    // 人工栈
+    helpStack := new(LinkStack)
+    // 第一次初始化栈，推入下标0，len(array)-1，表示第一次对全数组范围切分
+    helpStack.Push(len(array) - 1)
+    helpStack.Push(0)
+    // 栈非空证明存在未排序的部分
+    for !helpStack.IsEmpty() {
+        // 出栈，对begin-end范围进行切分排序
+        begin := helpStack.Pop() // 范围区间左边
+        end := helpStack.Pop()   // 范围
+        // 进行切分
+        loc := partition(array, begin, end)
+        // 切分后右边范围大小
+        rSize := -1
+        // 切分后左边范围大小
+        lSize := -1
+        // 右边范围入栈
+        if loc+1 < end {
+            rSize = end - (loc + 1)
+        }
+        // 左边返回入栈
+        if begin < loc-1 {
+            lSize = loc - 1 - begin
+        }
+        // 两个范围，让范围小的先入栈，减少人工栈空间
+        if rSize != -1 && lSize != -1 {
+            if lSize > rSize {
+                helpStack.Push(end)
+                helpStack.Push(loc + 1)
+                helpStack.Push(loc - 1)
+                helpStack.Push(begin)
+            } else {
+                helpStack.Push(loc - 1)
+                helpStack.Push(begin)
+                helpStack.Push(end)
+                helpStack.Push(loc + 1)
+            }
+        } else {
+            if rSize != -1 {
+                helpStack.Push(end)
+                helpStack.Push(loc + 1)
+            }
+            if lSize != -1 {
+                helpStack.Push(loc - 1)
+                helpStack.Push(begin)
+            }
+        }
+    }
+}
